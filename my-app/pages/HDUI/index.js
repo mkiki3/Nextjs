1b637348eeedbd2701e3966535ee72045d2f0ae5
Kiersten Magee <kierstenmagee@Dialexas-MacBook-Pro.local>
2021-11-11 10:16:19 -0600

    edit package.json

---
+++ my-app/pages/HDUI/index.js
@@ -0,0 +1,13 @@
+// our-domain.com/blog/something-important
+import Head from 'next/head'
+import Tabs from '../../components/Tabs'
+
+export default function Blog() {
+    return (
+      <div>
+          <Head> Headless Ui and Tailwind </Head>
+        <Tabs/>
+      </div>
+    )
+  }
+  
